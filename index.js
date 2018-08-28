4a9784871976581c34ae63b388cef7f8e158bfbe
ZiiWaitiiing <42684743+ZiiWaitiiing@users.noreply.github.com>
2018-08-28 21:33:11 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const Discord = require("bot.js")
+
+var bot = new.Discord.Client() {
+	bot.usersetGame("Préfix !")
+	console.log("Le bot a bien été connéctez!")
+});
+
+bot.login("NDgyNTQwNjA5MjcwMzE3MDU2.Dmc0vQ.2jU5QFj8-Vuqd-2xUWOiH7dPz44");
